6b8071d566755ecb879dcf47e284a1e5defd2b8f
splashcad <kevin@advancedglass.ie>
2026-08-16 09:43:26 +0100

    Simplify field outline workflow and render real notches

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='splashcad-fieldfix-20260816092438';
+const CACHE='splashcad-field-flow-20260816094325';
 const CORE=['/','/index.html','/hob.html','/window.html','/styles.css','/splashcad-app.js','/window-wall.js','/voice.js','/tablet.js','/manifest.webmanifest','/splashcad-icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
