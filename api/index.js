9d620312fe7e75d1fce0ebd94e5fc3da18a98f21
splashcad <kevin@advancedglass.ie>
2026-08-19 07:26:16 +0100

    Recovery R006 preserve original path through isolated Vercel route

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
 import handler from "../server.js";
 
-export default handler;
+export default function recoveryRoute(req, res) {
+  const url = new URL(req.url || "/", `http://${req.headers.host || "localhost"}`);
+  const recoveryPath = url.searchParams.get("recoveryPath");
+  if (recoveryPath) req.url = `/${recoveryPath}`;
+  return handler(req, res);
+}
